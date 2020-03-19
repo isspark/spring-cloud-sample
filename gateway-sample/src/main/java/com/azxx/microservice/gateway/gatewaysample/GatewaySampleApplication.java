cdb3adb6c13ef3d76a93289a17f22710c595c057
xuzheng <hx0011001@hengxinyongli.com>
2020-03-19 10:59:49 +0800

    fix:update config

---
+++ gateway-sample/src/main/java/com/azxx/microservice/gateway/gatewaysample/GatewaySampleApplication.java
@@ -0,0 +1,13 @@
+package com.azxx.microservice.gateway.gatewaysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySampleApplication.class, args);
+    }
+
+}
